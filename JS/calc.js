6d55bed56783bb4fb323428cccaa5b5cf82839ef
Reem Asaad <reemasaad48@gmail.com>
2020-09-29 15:31:09 +0300

    modifying js file

--- JS/calc.js
+++ JS/calc.js
@@ -1,5 +1,62 @@
 'use strict';
 
+// Get the modal
+var modal = document.getElementsByClassName('modal');
+
+
+function getSelectValue() {
+    var selectedValue = document.getElementById("list").value;
+    if(selectedValue == 'diabetes') {
+        modal[0].style.display = "block";
+    } else if(selectedValue == 'kidney') {
+        modal[1].style.display = "block";
+    } else if(selectedValue == 'calorie') {
+        modal[2].style.display = "block";
+    }
+}
+getSelectValue();
+
+
+// Get the button that opens the modal
+// var btn = document.getElementsByClassName("myBtn");
+
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close");
+
+// When the user clicks the button, open the modal 
+// btn[0].onclick = function() {
+//     modal[0].style.display = "block";
+// }
+
+// btn[1].onclick = function() {
+//     modal[1].style.display = "block";
+// }
+
+// btn[2].onclick = function() {
+//     modal[2].style.display = "block";
+// }
+
+// When the user clicks on <span> (x), close the modal
+span[0].onclick = function() {
+    modal[0].style.display = "none";
+}
+
+span[1].onclick = function() {
+    modal[1].style.display = "none";
+}
+
+span[2].onclick = function() {
+    modal[2].style.display = "none";
+}
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+
 var inputDiabetes = document.getElementById('inputDiabetes');
 var buttonDiabetes = document.getElementById('diabetesSubmit');
 buttonDiabetes.addEventListener('click', diabetes);
@@ -32,3 +89,26 @@ function kidney(event) {
     }
 }
 
+var man = document.getElementById('male');
+var woman = document.getElementById('female');
+var weight = document.getElementById('weight');
+var height = document.getElementById('height');
+var age = document.getElementById('age');
+var buttonCalorie = document.getElementById('calorieSubmit');
+var labelC = document.getElementById('result3');
+
+buttonCalorie.addEventListener('click', calorie);
+
+function calorie(event) {
+    event.preventDefault();   // to prevent refresh the browser 
+    if (man.checked) {
+        var kcalMan = (10 * weight.value) + (6.25 * height.value) - (5 * age.value) + 5;
+        labelC.innerHTML = kcalMan;
+
+    } else if (woman.checked) {
+
+        var kcalWoman = (10 * weight.value) + (6.25 * height.value) - (5 * age.value) - 161;
+        labelC.innerHTML = kcalWoman;
+
+    }
+}

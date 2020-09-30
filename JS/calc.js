235a544c3140f3bc1d3de8a19ac820b38fe8f4b2
Hadi Aji <hadeaji@gmail.com>
2020-09-30 19:50:26 +0300

    more optimize

--- JS/calc.js
+++ JS/calc.js
@@ -18,42 +18,30 @@ function getSelectValue() {
 getSelectValue();
 
 
-// Get the button that opens the modal
-// var btn = document.getElementsByClassName("myBtn");
-
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close");
 
-// When the user clicks the button, open the modal 
-// btn[0].onclick = function() {
-//     modal[0].style.display = "block";
-// }
-
-// btn[1].onclick = function() {
-//     modal[1].style.display = "block";
-// }
-
-// btn[2].onclick = function() {
-//     modal[2].style.display = "block";
-// }
-
 // When the user clicks on <span> (x), close the modal
 span[0].onclick = function() {
     modal[0].style.display = "none";
+    document.getElementById("result1").textContent = '';
 }
 
 span[1].onclick = function() {
     modal[1].style.display = "none";
+    document.getElementById("result2").textContent = '';
 }
 
 span[2].onclick = function() {
     modal[2].style.display = "none";
+    document.getElementById("result3").textContent = '';
 }
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
+        
     }
 }
 
@@ -69,6 +57,7 @@ function diabetes(event) {
     var diabetesEquation = Math.floor(0.3 * inputDiabetes.value);
     console.log(diabetesEquation);
     labelD.innerHTML = diabetesEquation + ' units of insulin.'  ;
+    document.getElementById("form1m").reset();
 }
 
 
@@ -80,14 +69,17 @@ buttonKidney.addEventListener('click', kidney);
 
 function kidney(event) {
     event.preventDefault();
-    if (radioInput1.checked) {
+    if ((radioInput1.checked ||radioInput2.checked)&& inputKidney.value===''){
+    alert('fill all the fields please!');}
+    else if (radioInput1.checked) {
         var preDialysis = 0.6 * inputKidney.value;
-        labelK.innerHTML = preDialysis+ ' g of protein';
+        labelK.innerHTML = parseFloat(preDialysis).toFixed(2) + 'g Of Protein You Need To Take Per Day.';
     }
     else if (radioInput2.checked) {
         var dialysis = 1.2 * inputKidney.value;
-        labelK.innerHTML = dialysis + ' g of protein.';
+        labelK.innerHTML = dialysis + 'g Of Protein You Need To Take Per Day.';
     }
+    document.getElementById("form2m").reset();
 }
 
 var man = document.getElementById('male');
@@ -102,19 +94,28 @@ buttonCalorie.addEventListener('click', calorie);
 
 function calorie(event) {
     event.preventDefault();   // to prevent refresh the browser 
-    if (man.checked) {
+    if (!(man.checked) && !(woman.checked)){
+        alert('Please choose your gender!');
+
+    } else if ((man.checked ||woman.checked)&& weight.value==='' || height.value==='' || age.value===''){
+        alert('fill all the fields please!');
+    }else if (man.checked) {
         var kcalMan = (10 * weight.value) + (6.25 * height.value) - (5 * age.value) + 5;
         labelC.innerHTML = kcalMan + ' kcal / day ';
 
     } else if (woman.checked) {
+        console.log(weight.value);
+        console.log(height.value);
+        console.log(5 * age.value);
 
         var kcalWoman = (10 * weight.value) + (6.25 * height.value) - (5 * age.value) - 161;
         labelC.innerHTML = kcalWoman+ ' kcal / day';
 
     }
+    document.getElementById("form3m").reset();
 }
 
 function resetSelectElement(selectElement) {
-    document.getElementById('list').selectedIndex = 0;  // first option is selected, or
-                                     // -1 for no option selected
+document.getElementById('list').selectedIndex = 0;  // first option is selected, or
+ // -1 for no option selected
 }
